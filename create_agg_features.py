6ab807ac852c4dfe74fc29c50b3e889f274b8342
Christopher McGale <56483395+chrismcgale@users.noreply.github.com>
2023-08-05 15:07:41 -0400

    Fix to agg data and started random forest

--- create_agg_features.py
+++ create_agg_features.py
@@ -12,7 +12,17 @@ def moving_average(arr, window_size):
 
     return moving_avg
 
-for old_file_path in glob.glob('.\data\\test_FD00*.txt') + glob.glob('.\data\\train_FD00*.txt'):
+files = glob.glob('.\data\\test_FD00[0-9].txt') + glob.glob('.\data\\train_FD00[0-9].txt')
+    
+for old_file_path in files:
+    new_file_path = old_file_path.replace('.txt', '_agg.txt')
+        
+    # Delete the file if exists already
+    if os.path.exists(new_file_path):
+        os.remove(new_file_path)
+        
+
+for old_file_path in files:
     with open(old_file_path, newline='') as file:
         new_file_path = old_file_path.replace('.txt', '_agg.txt')
         
@@ -27,14 +37,29 @@ for old_file_path in glob.glob('.\data\\test_FD00*.txt') + glob.glob('.\data\\tr
         window_values = []
 
         for line in csv_reader.to_numpy():
-            # Reset window for new device
-            if line[0] != curr_id:
-                curr_id = line[0]
-                window_values = []
-                
+            
             # Incase of file end
             if np.issubdtype(line.dtype, object):
                 break
+
+            if line[0] != curr_id and curr_id != -1:
+                stacked_array = np.vstack(window_values)
+                std_dev_by_index = np.std(stacked_array, axis=0)
+                moving_avg = np.sum(stacked_array, axis=0) / len(window_values)
+                
+                # Empty window
+                while window_values:
+                    # Output to new file
+                    with open(new_file_path, 'a') as new_file:
+                        # Only output aggregate values so we're not repeating
+                        np.savetxt(new_file, np.append(moving_avg, std_dev_by_index), delimiter=' ', newline=" ", fmt="%.3f")
+                        new_file.write('\n')
+                    window_values.pop(0)
+                
+                # Reset window for new device
+                curr_id = line[0]
+                window_values = []
+                
                 
             sensor_data = line[5:26]
             window_values.append(sensor_data)
@@ -52,5 +77,21 @@ for old_file_path in glob.glob('.\data\\test_FD00*.txt') + glob.glob('.\data\\tr
                 # Output to new file
                 with open(new_file_path, 'a') as new_file:
                     # Only output aggregate values so we're not repeating
-                    np.savetxt(new_file, np.append(moving_avg, std_dev_by_index), delimiter=' ', newline=",", fmt="%.3f")
+                    np.savetxt(new_file, np.append(moving_avg, std_dev_by_index), delimiter=' ', newline=" ", fmt="%.3f")
                     new_file.write('\n')
+                    
+        if len(window_values) > 0:
+                    
+            stacked_array = np.vstack(window_values)
+            std_dev_by_index = np.std(stacked_array, axis=0)
+            moving_avg = np.sum(stacked_array, axis=0) / len(window_values)
+            
+            # Empty window
+            while window_values:
+                # Output to new file
+                with open(new_file_path, 'a') as new_file:
+                    # Only output aggregate values so we're not repeating
+                    np.savetxt(new_file, np.append(moving_avg, std_dev_by_index), delimiter=' ', newline=" ", fmt="%.3f")
+                    new_file.write('\n')
+                window_values.pop(0)
+                
